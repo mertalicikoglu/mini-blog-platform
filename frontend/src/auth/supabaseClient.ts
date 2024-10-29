ffb322590c072ef5f99bd5c03f8b2677ce442194
MERT ALİCİKOĞLU <alicikoglumert@gmail.com>
2024-10-30 00:13:45 +0300

    connection bug fix

--- frontend/src/auth/supabaseClient.ts
+++ frontend/src/auth/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.VITE_SUPABASE_URL!;
-const SUPABASE_ANON_KEY = process.env.VITE_SUPABASE_KEY!;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(supabaseUrl, supabaseKey);
