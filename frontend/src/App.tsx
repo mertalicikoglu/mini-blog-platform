14fea75f9348a74c7c54b45ec392cd6cd951b2fb
MERT ALİCİKOĞLU <alicikoglumert@gmail.com>
2024-10-29 18:12:53 +0300

    add post detail route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,27 @@
-import { AuthProvider } from './auth/AuthProvider';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './auth/AuthProvider';
+import SignIn from './auth/SignIn';
+import SignUp from './auth/SignUp';
+import CreatePost from './components/CreatePost';
+import Posts from './components/Posts';
+import PostDetail from './components/PostDetail';
+import ProtectedRoute from './auth/ProtectedRoute';
 
-import './App.css'
-
-function App() {
-
+const App: React.FC = () => {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          {/* Rotasyonları burada tanımlayabilirsiniz */}
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/create-post" element={<ProtectedRoute element={<CreatePost />} path="/create-post" />} />
+          <Route path="/post/:postId" element={<PostDetail />} /> {/* PostDetail rotası tanımlandı */}
+          <Route path="/" element={<Posts />} />
         </Routes>
       </Router>
     </AuthProvider>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
