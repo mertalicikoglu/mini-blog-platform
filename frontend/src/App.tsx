92da3a871a8b17a2bf13e4f309c55dfbbe2043d8
MERT ALİCİKOĞLU <alicikoglumert@gmail.com>
2024-10-30 00:12:39 +0300

    add bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './auth/AuthProvider';
 import SignIn from './auth/SignIn';
@@ -7,20 +8,32 @@ import CreatePost from './components/CreatePost';
 import Posts from './components/Posts';
 import PostDetail from './components/PostDetail';
 import ProtectedRoute from './auth/ProtectedRoute';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/create-post" element={<ProtectedRoute element={<CreatePost />} path="/create-post" />} />
-          <Route path="/post/:postId" element={<PostDetail />} /> {/* PostDetail rotası tanımlandı */}
-          <Route path="/" element={<Posts />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route
+              path="/create-post"
+              element={
+                <ProtectedRoute>
+                  <CreatePost />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/post/:postId" element={<PostDetail />} /> {/* PostDetail rotası tanımlandı */}
+            <Route path="/" element={<Posts />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 };
 
