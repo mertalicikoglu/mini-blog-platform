d7645a4ccfb6543643f4c80c17bb54bc7ca49fec
MERT ALİCİKOĞLU <alicikoglumert@gmail.com>
2024-10-30 00:08:58 +0300

    add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import postRoutes from './routes/postRoutes';
 import commentRoutes from './routes/commentRoutes';
-import authRoutes from './auth/authRoutes';
+//import { initializeDatabase } from './initializeDatabase';
 import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json()); // JSON desteği
-app.use('/api/auth', authRoutes); // Kimlik doğrulama rotaları
+//initializeDatabase();
 app.use('/api/posts', postRoutes); // Post rotaları
 app.use('/api/posts/:postId/comments', commentRoutes); // Comment rotaları
 
