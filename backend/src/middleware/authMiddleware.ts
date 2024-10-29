9741826d6ce18a68b0f3d6763693be40366b0827
MERT ALİCİKOĞLU <alicikoglumert@gmail.com>
2024-10-30 00:10:30 +0300

    add auth middleware

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,25 +1,25 @@
-// import { Request, Response, NextFunction } from 'express';
-// import { supabase } from '../auth/supabaseClient';
+import { Request, Response, NextFunction } from 'express';
+import { supabase } from '../auth/supabaseClient';
 
-// export const requireAuth = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-//   const authHeader = req.headers['authorization'];
-//   if (!authHeader) {
-//     res.status(401).json({ error: 'Authorization header is missing' });
-//     return; // Yanıt verildikten sonra işlevden erken çıkıyoruz
-//   }
+export const requireAuth = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) {
+    res.status(401).json({ error: 'Authorization header is missing' });
+    return; // Yanıt verildikten sonra işlevden erken çıkıyoruz
+  }
 
-//   const token = authHeader.split(' ')[1];
-//   if (!token) {
-//     res.status(401).json({ error: 'Token is missing' });
-//     return; // Yanıt verildikten sonra işlevden erken çıkıyoruz
-//   }
+  const token = authHeader.split(' ')[1];
+  if (!token) {
+    res.status(401).json({ error: 'Token is missing' });
+    return; // Yanıt verildikten sonra işlevden erken çıkıyoruz
+  }
 
-//   const { data, error } = await supabase.auth.getUser(token);
-//   if (error || !data?.user) {
-//     res.status(401).json({ error: 'Unauthorized' });
-//     return; // Yanıt verildikten sonra işlevden erken çıkıyoruz
-//   }
+  const { data, error } = await supabase.auth.getUser(token);
+  if (error || !data?.user) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return; // Yanıt verildikten sonra işlevden erken çıkıyoruz
+  }
 
-//   req.user = data.user; // Kullanıcıyı request objesine ekliyoruz.
-//   next(); // İşleve devam ediyoruz, bu durumda void dönecek
-// };
+  req.user = data.user; // Kullanıcıyı request objesine ekliyoruz.
+  next(); // İşleve devam ediyoruz, bu durumda void dönecek
+};
