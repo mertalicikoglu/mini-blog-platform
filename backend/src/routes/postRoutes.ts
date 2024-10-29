03a86587a353baa4e8481419e9ad42015a5e4d01
MERT ALİCİKOĞLU <alicikoglumert@gmail.com>
2024-10-29 17:36:40 +0300

    update post route

--- backend/src/routes/postRoutes.ts
+++ backend/src/routes/postRoutes.ts
@@ -1,23 +1,28 @@
 import express from 'express';
-import { getPosts, getPostById, createPost, updatePost, deletePost } from '../controllers/postController';
-import { requireAuth } from '../middleware/authMiddleware'; // Kimlik doğrulama kontrolü için middleware
+import { getPosts, getPostById, createPost, updatePost, deletePost, searchPosts } from '../controllers/postController';
+//import { requireAuth } from '../middleware/authMiddleware'; // Kimlik doğrulama kontrolü için middleware
 import { isPostOwner } from '../middleware/authorizationMiddleware'; // Gönderi sahibi kontrolü için middleware
 
 const router = express.Router();
 
-// Tüm gönderileri getir
-router.get('/', getPosts);
+// // Tüm gönderileri getir
+// router.get('/', getPosts);
+
+// Gönderileri arama
+router.get('/', searchPosts);
 
 // Belirli bir gönderiyi getir
 router.get('/:id', getPostById);
 
 // Yeni gönderi oluşturma (sadece kimliği doğrulanmış kullanıcılar)
-router.post('/', requireAuth, createPost);
+router.post('/', createPost);
 
 // Belirli bir gönderiyi güncelleme (sadece gönderi sahibi)
-router.put('/:id', requireAuth, isPostOwner, updatePost);
+router.put('/:id', isPostOwner, updatePost);
 
 // Belirli bir gönderiyi silme (sadece gönderi sahibi)
-router.delete('/:id', requireAuth, isPostOwner, deletePost);
+router.delete('/:id', isPostOwner, deletePost);
+
+
 
 export default router;
