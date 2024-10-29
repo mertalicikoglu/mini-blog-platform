61e53f38308f31a2f940e3cf07ebdccbeacf0bc9
MERT ALİCİKOĞLU <alicikoglumert@gmail.com>
2024-10-29 17:33:12 +0300

    auth controlled on fe, so no need for this

--- backend/src/auth/authRoutes.ts
+++ backend/src/auth/authRoutes.ts
@@ -1,21 +1,21 @@
-// src/auth/authRoutes.ts
+// // src/auth/authRoutes.ts
 
-import express from 'express';
-import { signUp, signIn, signOut } from './authController';
+// import express from 'express';
+// import { signUp, signIn, signOut } from './authController';
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post('/signup', async (req, res) => {
-    let data = await signUp(req, res);
-    res.json(data);
-});   // Kayıt rotası
-router.post('/signin', async (req, res) => {
-    let data = await signIn(req, res);
-    res.json(data);
-});   // Giriş rotası
-router.post('/signout', async (req, res) => {
-    let data = await signOut(req, res);
-    res.json(data);
-}); // Çıkış rotası
+// router.post('/signup', async (req, res) => {
+//     let data = await signUp(req, res);
+//     res.json(data);
+// });   // Kayıt rotası
+// router.post('/signin', async (req, res) => {
+//     let data = await signIn(req, res);
+//     res.json(data);
+// });   // Giriş rotası
+// router.post('/signout', async (req, res) => {
+//     let data = await signOut(req, res);
+//     res.json(data);
+// }); // Çıkış rotası
 
-export default router;
+// export default router;
