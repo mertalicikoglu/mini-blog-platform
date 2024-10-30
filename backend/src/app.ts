ef831638fc98a3d235a0030ae3d36799dc4ee0be
MERT ALİCİKOĞLU <alicikoglumert@gmail.com>
2024-10-30 03:15:41 +0300

    change route model

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request } from 'express';
 import cors from 'cors';
 import postRoutes from './routes/postRoutes';
 import commentRoutes from './routes/commentRoutes';
@@ -9,8 +9,18 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // JSON desteği
 //initializeDatabase();
+
+// TypeScript için postId özelliğini Request türüne eklemek
+interface CustomRequest extends Request {
+  postId?: string;
+}
+
+app.use('/api/posts/:postId/comments', (req, res, next) => {
+  (req as CustomRequest).postId = req.params.postId;
+  next();
+}, commentRoutes); // Comment rotaları
+
 app.use('/api/posts', postRoutes); // Post rotaları
-app.use('/api/posts/:postId/comments', commentRoutes); // Comment rotaları
 
 // Global hata yakalama middleware'i
 app.use(errorHandler);
